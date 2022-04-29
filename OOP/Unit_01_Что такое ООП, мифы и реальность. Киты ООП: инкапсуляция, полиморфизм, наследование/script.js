053faad9ea30c01e1e8f05afd09cecd4e57bb593
Vctr-Ost <102282646+Vctr-Ost@users.noreply.github.com>
2022-04-29 15:42:28 +0300

    Add files via upload

---
+++ OOP/Unit_01_Что такое ООП, мифы и реальность. Киты ООП: инкапсуляция, полиморфизм, наследование/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+let user = {
+	userName: '',
+	userPass: '',
+	userEmail: '',
+	userRole: '',
+	// методы
+	// сбросить пароль ()
+	// getCourses(
+			// if(admin) делай одно
+			// if(student) делай другое
+			// if(guest) делай третье
+	// )
+}
+
+admin (user) = {
+	// changeUserStatus()
+	// changeUserCourses()
+	// getCourses()		// перезаписать метод (получить все курсы)
+
+}
+
+student (user) {
+	// achivements()
+	// getCourses()		// перезаписать метод (получить курсы конкретного студента)
+}
+
+
+
+
+
+
+
+
+let news = {
+	date = new Date(),
+}
+
+item (news) = {
+	theme: '',
+	title: '',
+	subTitle: '',
+	text: '',
+}

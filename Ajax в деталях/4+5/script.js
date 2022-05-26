0136fd0c396c18766b68576339e3de212f36c9ab
Viktor Ostapenko <102282646+Vctr-Ost@users.noreply.github.com>
2022-05-26 21:17:52 +0300

    Create script.js

---
+++ Ajax в деталях/4+5/script.js
@@ -0,0 +1,18 @@
+let inputCityID = document.querySelector('.input');
+
+
+document.querySelector('.btn1').addEventListener('click', () => {
+	const promise = getWeather(inputCityID.value);
+	promise.then(callBackFunc)
+})
+
+function callBackFunc(data) {
+	console.log(data.name + ' - ' + Math.round(data.main.temp - 273) + 'grad');
+}
+
+
+
+
+//* 703448 - kyiv
+//* 706483 - kharkiv
+//* 702550 - lviv
